34e283ef578e74b4befa569fbf877eccc5571d80
Lemuel Bodi-Ngwala <lembn@outlook.com>
2022-10-18 17:29:43 +0100

    removed leftover redundant code

--- primes.py
+++ primes.py
@@ -14,7 +14,6 @@ def primes(number_of_primes):
     while len(prime_numbers) < number_of_primes:
         for p in [p for p in prime_numbers if p <= ceil(n / 2)]:
             if not n % p:
-                is_prime = False
                 break 
         else:
             prime_numbers.append(n)
