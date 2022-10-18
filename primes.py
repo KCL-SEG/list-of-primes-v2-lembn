5754a11cbd9cf337865927777b80656f2f8b2e92
Lemuel Bodi-Ngwala <lembn@outlook.com>
2022-10-18 17:20:45 +0100

    attempt 1

--- primes.py
+++ primes.py
@@ -1,6 +1,29 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from math import ceil
+
+
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise ValueError("Number of primes must be > 0.")
+
+    prime_numbers = [2]
+    n = 3
+
+    while len(prime_numbers) < number_of_primes:
+        is_prime = True
+        for p in [p for p in prime_numbers if p <= ceil(n / 2)]:
+            if not n % p:
+                is_prime = False
+                break 
+
+        if is_prime:
+            prime_numbers.append(n)
+        
+        n += 1
+
+    return prime_numbers
+
+if __name__ == "__main__":
+    print(primes(20))
